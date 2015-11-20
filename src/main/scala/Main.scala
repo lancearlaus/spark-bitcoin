e4551b9a5f4c2212c0d5010b84cab24cd5049323
Lance Arlaus <lance.arlaus@gmail.com>
2015-11-20 14:45:03 -0500

    Working interim version

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -11,10 +11,11 @@ object Main {
 
     val magic = 0xD9B4BEF9L
     val version = 1
-    //    val file = "data/bootstrap.dat"
-    val file = "data/blk00000.dat"
+    val file = "data/bootstrap.dat"
+    //val file = "data/blk00000.dat"
 
-    val codec = Blockchain.codec(magic, version)
+    val codec = Blockchain(magic, 1).BlockHeaders.decoder
+//    val codec = Blockchain.codec(magic, version)
     val stream = new FileInputStream(file)
     val bits = BitVector.fromInputStream(stream)
 
@@ -23,25 +24,36 @@ object Main {
     var (blockCount, txCount) = (0L, 0L)
     var remainder = bits
 
-    while (remainder.nonEmpty) {
-      codec.decode(remainder) match {
-        case Successful(result) => {
-          remainder = result.remainder
-          blockCount = blockCount + 1
-          //txCount = txCount + result.value.block.txs.size
-          //          println(s"count: $count, frame: ${result.value}")
-          print(".")
-          if (blockCount % 100 == 0) println(s" -> blocks: $blockCount, txs: $txCount")
-        }
-        case Failure(cause) => {
-          println(s"FAILED: $cause")
-          remainder = BitVector.empty
+    try {
+      while (remainder.nonEmpty) {
+        codec.decode(remainder) match {
+          case Successful(result) => {
+            remainder = result.remainder
+            blockCount = blockCount + 1
+            //txCount = txCount + result.value.block.txs.size
+            //          println(s"count: $count, frame: ${result.value}")
+            print(".")
+            if (blockCount % 100 == 0) println(s" -> blocks: $blockCount, txs: $txCount")
+          }
+          case Failure(cause) => {
+            println(s"FAILED: $cause")
+            remainder = BitVector.empty
+          }
         }
       }
+    } finally {
+      stream.close()
     }
 
     println(s"Totals: blocks: $blockCount, txs: $txCount")
 
+    println(s"Calculating index...")
+
+    val stream2 = new FileInputStream(file)
+    Blockchain(magic, version).entries(BitVector.fromInputStream(stream2)) match {
+      case Left(error) => println(s"ERROR: $error")
+      case Right(entries) => println(s"Entries size: ${entries.size}")
+    }
   }
 
 }
--- src/main/scala/io/github/lancearlaus/bitcoin/BlockChain.scala
+++ src/main/scala/io/github/lancearlaus/bitcoin/BlockChain.scala
@@ -1,32 +1,103 @@
 package io.github.lancearlaus.bitcoin
 
 import io.github.yzernik.bitcoinscodec.messages.Block
-import scodec.Codec
+import scodec.Attempt.{Failure, Successful}
+import scodec.{Decoder, Codec}
 import scodec.bits.{BitVector, ByteOrdering}
 import scodec.codecs._
+import scala.annotation.tailrec
+import scala.collection.mutable
+
+case class Blockchain(magic: Long, version: Int) {
+
+  val blockCodec = Block.codec(version)
+
+  case class BlockHeader(length: Long)
+
+  object BlockHeader {
+    val length = 8   // bytes
+    val codec = (
+      constant(BitVector.fromLong(magic, 32, ByteOrdering.LittleEndian)) :~>:
+      ("length" | uint32L)
+    ).as[BlockHeader]
+  }
+
+  object BlockHeaders {
+
+    // Decodes headers without decoding blocks
+    val decoder = Decoder[BlockHeader] { data: BitVector =>
+      for {
+        header <- BlockHeader.codec.decode(data)
+        skip <- bits(header.value.length * 8).decode(header.remainder)
+      } yield header.mapRemainder(r => skip.remainder)
+    }
+
+  }
+
+  case class BlockEntry(offset: Long, length: Long) {
+    def end = offset + length
+  }
+  object BlockEntry {
+    def apply(offset: Long, header: BlockHeader): BlockEntry = BlockEntry(offset + BlockHeader.length, header.length)
+  }
 
 
-object Blockchain {
-
-  def codec(magic: Long, version: Int): Codec[Block] = {
-
-    val magicCodec = constant(BitVector.fromLong(magic, 32, ByteOrdering.LittleEndian))
-    val blockCodec = Block.codec(version)
-
+  val codec: Codec[Block] = {
     def encode(block: Block) = for {
-      magic <- magicCodec.encode(())
       payload <- blockCodec.encode(block)
-      length <- uint32L.encode(payload.size / 8)
-    } yield magic ++ length ++ payload
+      header <- BlockHeader.codec.encode(BlockHeader(payload.length / 8))
+    } yield header ++ payload
 
-    def decode(data: BitVector) = for {
-      magic <- magicCodec.decode(data)
-      length <- uint32L.decode(magic.remainder)
-      payload <- bits(length.value * 8).decode(length.remainder)
-      block <- blockCodec.decode(payload.value)
-    } yield block.mapRemainder(_ => payload.remainder)
+    def decode(bits: BitVector) = for {
+      header <- BlockHeader.codec.decode(bits)
+      // Split to ensure block decode doesn't consume more bits than stated
+      (payload, remainder) = header.remainder.splitAt(header.value.length * 8)
+      block <- blockCodec.decode(payload)
+    } yield block.mapRemainder(_ => remainder)
 
     Codec[Block](encode _, decode _)
+  }
+
+  // Extract all block locations (without parsing the blocks
+  def entries(bits: BitVector): Either[String, List[BlockEntry]] = {
+
+    // TODO: Figure out why this recursive function leads to OutOfMemoryError
+    @tailrec
+    def decode(bits: BitVector, offset: Long, entries: mutable.MutableList[BlockEntry] = mutable.MutableList.empty): Either[String, List[BlockEntry]] = {
+      if (bits.nonEmpty) {
+        (for {
+          header <- BlockHeaders.decoder.decode(bits)
+//          entry =
+        } yield (BlockEntry(offset, header.value), header.remainder)) match {
+          case Successful((entry, tail)) => decode(tail, entry.end, entries += entry)
+          case Failure(cause) => Left(cause.messageWithContext)
+        }
+      } else {
+        Right(entries.toList)
+      }
+    }
+    //    decode(bits, 0)
+
+    var offset = 0L
+    var tail = bits
+    val entries = mutable.MutableList.empty[BlockEntry]
+
+    while (tail.nonEmpty) {
+      BlockHeaders.decoder.decode(tail) match {
+        case Successful(result) => {
+          val entry = BlockEntry(offset, result.value)
+          entries += entry
+          tail = result.remainder
+          offset = entry.end
+        }
+        case Failure(cause) => {
+          println(s"ERROR: ${cause.messageWithContext}")
+          tail = BitVector.empty
+        }
+      }
+    }
+
+    Right(entries.toList)
   }
 
 }
